4d2256453c0d886bccb96ac349d0090025769752
Ramsey Ith Njema Jr <ramseyith99@gmail.com>
2020-08-31 14:36:14 +0200

    Initial customiztion

---
+++ src/dist/index.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+require("bootstrap/dist/css/bootstrap.min.css");
+
+var _jquery = _interopRequireDefault(require("jquery"));
+
+var _popper = _interopRequireDefault(require("popper.js"));
+
+require("bootstrap/dist/js/bootstrap.bundle.min");
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
